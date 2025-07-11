04889a043cbaaba3fd23c0f3081223657d8cb943
aki <aki.grosjean@gmail.com>
2025-07-11 12:32:51 +0900

    Move constants to separate file

---
+++ HandEvaluator/HandConstant.cs
@@ -0,0 +1,18 @@
+﻿namespace HandEvaluator;
+
+public partial class Hand : IComparable
+{
+    public static readonly int NumberOfCards = 52;
+
+    private static readonly int HANDTYPE_SHIFT = 24;
+    private static readonly int TOP_CARD_SHIFT = 16;
+    private static readonly uint TOP_CARD_MASK = 0x000F0000;
+    private static readonly int SECOND_CARD_SHIFT = 12;
+    private static readonly uint SECOND_CARD_MASK = 0x0000F000;
+    private static readonly int THIRD_CARD_SHIFT = 8;
+    private static readonly int FOURTH_CARD_SHIFT = 4;
+    private static readonly int FIFTH_CARD_SHIFT = 0;
+    private static readonly uint FIFTH_CARD_MASK = 0x0000000F;
+    private static readonly int CARD_WIDTH = 4;
+    private static readonly uint CARD_MASK = 0x0F;
+}
