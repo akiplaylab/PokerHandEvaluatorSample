c71685a69270edc85bb1791bec2946c081cbf1ac
aki <aki.grosjean@gmail.com>
2025-07-11 17:39:56 +0900

    Change to constant

--- HandEvaluator/Models/Rank.cs
+++ HandEvaluator/Models/Rank.cs
@@ -2,17 +2,17 @@
 
 public static class Rank
 {
-    public static readonly int Rank2 = 0;
-    public static readonly int Rank3 = 1;
-    public static readonly int Rank4 = 2;
-    public static readonly int Rank5 = 3;
-    public static readonly int Rank6 = 4;
-    public static readonly int Rank7 = 5;
-    public static readonly int Rank8 = 6;
-    public static readonly int Rank9 = 7;
-    public static readonly int RankTen = 8;
-    public static readonly int RankJack = 9;
-    public static readonly int RankQueen = 10;
-    public static readonly int RankKing = 11;
-    public static readonly int RankAce = 12;
+    public const int Rank2 = 0;
+    public const int Rank3 = 1;
+    public const int Rank4 = 2;
+    public const int Rank5 = 3;
+    public const int Rank6 = 4;
+    public const int Rank7 = 5;
+    public const int Rank8 = 6;
+    public const int Rank9 = 7;
+    public const int RankTen = 8;
+    public const int RankJack = 9;
+    public const int RankQueen = 10;
+    public const int RankKing = 11;
+    public const int RankAce = 12;
 }
