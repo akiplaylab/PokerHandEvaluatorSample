e4101f4e0883de6798d347614e8a683d120ef3bb
aki <aki.grosjean@gmail.com>
2025-07-16 18:51:30 +0900

    Add Hands test

---
+++ HandEvaluatorTests/HandIteratorTests.cs
@@ -0,0 +1,22 @@
+﻿using HandEvaluator;
+using System.Numerics;
+
+namespace HandEvaluatorTests;
+
+public partial class HandTests
+{
+    [TestMethod()]
+    public void HandsTest()
+    {
+        const int numberofCards = 5;
+        var result = Hand.Hands(numberofCards);
+        var expectedCount = 52 * 51 * 50 * 49 * 48 / (5 * 4 * 3 * 2 * 1); // C(52, 5)
+        Assert.AreEqual(expectedCount, result.Count(), "The number of combinations should match the expected count.");
+        Assert.IsTrue(result.All(hand => BitOperations.PopCount(hand) == numberofCards), "All hands should have exactly k cards.");
+        Assert.IsTrue(result.All(hand => hand < 1UL << 52), "All hands should be valid 52-card hands.");
+        Assert.IsTrue(result.Distinct().Count() == result.Count(), "All hands should be unique.");
+
+        ulong expectedFirstHand = 1UL << 51 | 1UL << 50 | 1UL << 49 | 1UL << 48 | 1UL << 47;
+        Assert.AreEqual(expectedFirstHand, result.First());
+    }
+}
