829498c33978a340d7c90ea158951bb72c2c22f7
aki <aki.grosjean@gmail.com>
2025-07-20 16:22:34 +0900

    Add test cases

--- HandEvaluator.Tests/HandIteratorTests.cs
+++ HandEvaluator.Tests/HandIteratorTests.cs
@@ -6,17 +6,32 @@ namespace HandEvaluator.Tests;
 public partial class HandTests
 {
     [TestMethod()]
-    public void HandsTest()
+    [DataRow(1, new ulong[] { 1UL << 0, 1UL << 51 })]
+    [DataRow(2, new ulong[] { 1UL << 1 | 1UL << 0, 1UL << 51 | 1UL << 50 })]
+    [DataRow(5, new ulong[] { 1UL << 4 | 1UL << 3 | 1UL << 2 | 1UL << 1 | 1UL << 0, 1UL << 51 | 1UL << 50 | 1UL << 49 | 1UL << 48 | 1UL << 47 })]
+    public void HandsTest(int numberOfCards, ulong[] contains)
     {
-        const int numberofCards = 5;
-        var result = Hand.Hands(numberofCards);
-        var expectedCount = Combinatorics.Combinations(52, numberofCards);
+        var result = Hand.Hands(numberOfCards);
+        var expectedCount = Combinatorics.Combinations(52, numberOfCards);
         Assert.AreEqual(expectedCount, result.Count(), "The number of combinations should match the expected count.");
-        Assert.IsTrue(result.All(hand => BitOperations.PopCount(hand) == numberofCards), "All hands should have exactly k cards.");
+        Assert.IsTrue(result.All(hand => BitOperations.PopCount(hand) == numberOfCards), "All hands should have exactly k cards.");
         Assert.IsTrue(result.All(hand => hand < 1UL << 52), "All hands should be valid 52-card hands.");
         Assert.IsTrue(result.Distinct().Count() == result.Count(), "All hands should be unique.");
 
-        ulong expectedFirstHand = 1UL << 51 | 1UL << 50 | 1UL << 49 | 1UL << 48 | 1UL << 47;
-        Assert.AreEqual(expectedFirstHand, result.First());
+        Assert.IsTrue(contains.All(c => result.Contains(c)), "The result should contain all expected hands.");
+    }
+
+    [TestMethod()]
+    [DataRow(5, 1UL << 51 | 1UL << 50 | 1UL << 1 | 1UL << 0, new ulong[] { 1UL << 6 | 1UL << 5 | 1UL << 4 | 1UL << 3 | 1UL << 2, 1UL << 49 | 1UL << 48 | 1UL << 47 | 1UL << 46 | 1UL << 45 })]
+    public void GetHandsTest_WithDead(int numberOfCards, ulong dead, ulong[] contains)
+    {
+        var result = Hand.Hands(0UL, dead, numberOfCards);
+        var expectedCount = Combinatorics.Combinations(52 - BitOperations.PopCount(dead), numberOfCards);
+        Assert.AreEqual(expectedCount, result.Count(), "The number of combinations should match the expected count.");
+        Assert.IsTrue(result.All(hand => BitOperations.PopCount(hand) == numberOfCards), "All hands should have exactly numberOfCards cards.");
+        Assert.IsTrue(result.All(hand => hand < 1UL << 52), "All hands should be valid 52-card hands.");
+        Assert.IsTrue(result.Distinct().Count() == result.Count(), "All hands should be unique.");
+
+        Assert.IsTrue(contains.All(c => result.Contains(c)), "The result should contain all expected hands.");
     }
 }
